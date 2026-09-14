32a0aeea38bab6bf620b46de7b904cb1b6e0ae7a
sunjae07 <tlatms0738@gmail.com>
2026-09-14 17:46:19 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,9 @@
-def print_hello():
-    print("Hello World!!")
+def add(x, y):
+    a = x+y
+    return a
 def print_kit():
-    print("Hello Kit!!")
-    
-if __name__ == '__main__':
-    print_hello()
+    print("kit")
+
+if __name__ == "__main__":
     print_kit()
+    print( add(2, 3) )
